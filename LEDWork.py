9d62536adfdc71f22ccd643bda5c1a94d701815a
SebTheRed <seb.belfonti@gmail.com>
2023-04-23 12:20:12 -0400

    Button now turns the red LED on or off.

--- LEDWork.py
+++ LEDWork.py
@@ -1,5 +1,8 @@
-from gpiozero import LED
+from gpiozero import LED, Button
 from time import sleep
+
+redButton = Button("GPIO18")
+
 class LEDOps:
 	def __init__(self):
 		self.yellowLED = LED("GPIO19")
@@ -43,11 +46,20 @@ class LEDOps:
 			sleep(1)
 			chosenLED.off()
 			sleep(1)
+	def buttonDown (self):
+		self.redLED.on()
+	def buttonUp (self):
+		self.redLED.off()
+		
 
 instanceLEDOps = LEDOps()
-methodOption = int(input("Please choose which method you would like to run:\n1. Light Show\n2. Blink\n"))
+methodOption = int(input("Please choose which method you would like to run:\n1. Light Show\n2. Blink\n3. Button\n"))
 if methodOption == 1:
 	instanceLEDOps.lightShow()
 elif methodOption == 2:
 	blinkInput = int(input("Please enter which LED you would like to see blink.\n1. Yellow\n2. Green\n3. Blue\n4. Red\n"))
 	instanceLEDOps.chooseALight(blinkInput)
+elif methodOption == 3:
+  redButton.when_pressed = instanceLEDOps.buttonDown()
+  redButton.when_released = instanceLEDOps.buttonUp()
+
