a8090041dae1b8823d723e3b640a0bbfbea809bf
SebTheRed <seb.belfonti@gmail.com>
2023-04-23 13:54:28 -0400

    LEDWork complete! Added methods for 4 buttons that turn on/off the 4 color LEDs.

--- LEDWork.py
+++ LEDWork.py
@@ -1,29 +1,32 @@
 from gpiozero import LED, Button
 from time import sleep
 
-redButton = Button("GPIO18")
+blueButton = Button("GPIO21")
+yellowButton = Button("GPIO6")
+greenButton = Button("GPIO5")
+redButton = Button("GPIO16")
 
 class LEDOps:
 	def __init__(self):
 		self.yellowLED = LED("GPIO19")
-		self.greenLED = LED("GPIO16")
-		self.redLED = LED("GPIO21")
-		self.blueLED = LED("GPIO13")
+		self.greenLED = LED("GPIO13")
+		self.redLED = LED("GPIO20")
+		self.blueLED = LED("GPIO26")
 	def lightShow(self):
 		print("Welcome to the light show!")
 		while True:
 			self.redLED.off()
-			self.yellowLED.on()
-			sleep(0.5)
-			self.yellowLED.off()
 			self.greenLED.on()
-			sleep(0.5)
+			sleep(0.1)
 			self.greenLED.off()
+			self.yellowLED.on()
+			sleep(0.1)
+			self.yellowLED.off()
 			self.blueLED.on()
-			sleep(0.5)
+			sleep(0.1)
 			self.blueLED.off()
 			self.redLED.on()
-			sleep(0.5)
+			sleep(0.1)
 	def chooseALight(self, inputVal):
 		chosenLED = ""
 		if inputVal == 1:
@@ -46,20 +49,42 @@ class LEDOps:
 			sleep(1)
 			chosenLED.off()
 			sleep(1)
-	def buttonDown (self):
+	def blueBtnDown (self):
+		self.blueLED.on()
+	def blueBtnUp (self):
+		self.blueLED.off()
+	def yellowBtnDown (self):
+		self.yellowLED.on()
+	def yellowBtnUp (self):
+		self.yellowLED.off()
+	def greenBtnDown (self):
+		self.greenLED.on()
+	def greenBtnUp (self):
+		self.greenLED.off()
+	def redBtnDown (self):
 		self.redLED.on()
-	def buttonUp (self):
+	def redBtnUp (self):
 		self.redLED.off()
+    
 		
 
 instanceLEDOps = LEDOps()
-methodOption = int(input("Please choose which method you would like to run:\n1. Light Show\n2. Blink\n3. Button\n"))
+methodOption = int(input("Please choose which method you would like to run:\n1. Light Show\n2. Blink\n3. Buttons\n"))
 if methodOption == 1:
 	instanceLEDOps.lightShow()
 elif methodOption == 2:
 	blinkInput = int(input("Please enter which LED you would like to see blink.\n1. Yellow\n2. Green\n3. Blue\n4. Red\n"))
 	instanceLEDOps.chooseALight(blinkInput)
 elif methodOption == 3:
-  redButton.when_pressed = instanceLEDOps.buttonDown()
-  redButton.when_released = instanceLEDOps.buttonUp()
+  while True:
+    blueButton.when_pressed = instanceLEDOps.blueBtnDown
+    blueButton.when_released = instanceLEDOps.blueBtnUp
 
+    yellowButton.when_pressed = instanceLEDOps.yellowBtnDown
+    yellowButton.when_released = instanceLEDOps.yellowBtnUp
+
+    greenButton.when_pressed = instanceLEDOps.greenBtnDown
+    greenButton.when_released = instanceLEDOps.greenBtnUp
+
+    redButton.when_pressed = instanceLEDOps.redBtnDown
+    redButton.when_released = instanceLEDOps.redBtnUp
