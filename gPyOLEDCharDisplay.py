a2f8e3a7004be9184db97fac5f08eb6d2fd95e0e
SebTheRed <seb.belfonti@gmail.com>
2023-05-08 22:36:45 -0400

    trial and error and failure

--- gPyOLEDCharDisplay.py
+++ gPyOLEDCharDisplay.py
@@ -1,6 +1,14 @@
-from gpiozero import LEDCharDisplay
+from gpiozero import LEDMultiCharDisplay, LEDCharDisplay
 import time
 
 class ChallengeFive:
     def __init__ (self):
-        self.var = 1
+        self.display = LEDMultiCharDisplay(LEDCharDisplay(22, 23, 24, 25, 21, 20, 16, dp=12),18, 24, 23, 4)
+        self.counter = 0
+    def runCounter (self):
+        while True:
+            self.counter += 1
+            self.display.text = str(self.counter)
+
+inst = ChallengeFive()
+inst.runCounter()
